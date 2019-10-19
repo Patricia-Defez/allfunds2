953ead46c5e23010a51311e0faf0110e5a369da2
Patricia Defez <ptddds@gmail.com>
2019-10-19 14:25:18 +0200

    navbar

---
+++ allfunds2/src/componentes/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default () => (
+    <nav className="navbar text-white"  role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <strong className="navbar-item">Allfunds Coding Challenge</strong>
+      </div>
+    </nav>
+);
