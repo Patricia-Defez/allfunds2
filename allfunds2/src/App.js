8bc54fa8bede0d65519552f8030588f103dc6eb0
Patricia Defez <ptddds@gmail.com>
2019-10-22 07:53:42 +0200

    input

--- allfunds2/src/App.js
+++ allfunds2/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './componentes/NavBar';
+import Form from './componentes/Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Form/>
     </div>
   );
 }
--- allfunds2/src/componentes/Form.js
+++ allfunds2/src/componentes/Form.js
@@ -6,10 +6,12 @@ import React, { useState, useCallback } from 'react';
 const Form = () => {
 
     const initialValues = {kind: "", quantity:0.00, resul: 0.00};
-    const [values, setValues] = useState(initialValues);
+    const [values, setValues] = useState({...initialValues});
 
-
-
+     const handleChange = (event) => {
+        event.persist();
+        setValues(values => ({ ...values, [event.target.name]: event.target.value }));
+    };
 
     function paying(n, limit, BFactor, SFactor) {
         let toPay = 0
@@ -20,14 +22,31 @@ const Form = () => {
                 toPay = (((n - limit) * SFactor) + (limit * BFactor)).toFixed(2)
             }
         }
-        return toPay
+        setValues(values => ({ ...values, [values.result]: toPay}))
+        return toPay;
     };
 
+    const  payment = function ({values}){
+       
+        switch ([values.kind]) {
+            case 'TRANSPORTATION':
+               return paying(values.quantity, 100, 0.12, 0.08)
+                
+            case 'MEAL':
+               return paying(values.quantity, 3, 10, 6)
+              
+            case 'PARKING':
+               return paying(values.quantity, 20, 1, 0.5)
+             
+            default:
+                return 0;
+        }
+    };
 
-    const toPayTransp = paying(transportation, 100, 0.12, 0.08)
-    const toPayMeal = paying(meal, 3, 10, 6)
-    const toPayParking = paying(parking, 20, 1, 0.5)
-    const toPayTotal = (toPayTransp * 1) + (toPayMeal * 1) + (toPayParking * 1)
+    // const toPayTransp = paying(transportation, 100, 0.12, 0.08)
+    // const toPayMeal = paying(meal, 3, 10, 6)
+    // const toPayParking = paying(parking, 20, 1, 0.5)
+    // const toPayTotal = (toPayTransp * 1) + (toPayMeal * 1) + (toPayParking * 1)
 
     return (
         <div className="box mx-auto p-5">
@@ -35,25 +54,32 @@ const Form = () => {
                 <h1 className="text-center">Gestion de Dietas</h1>
                 <div className="marco mx-auto m-5 p-5 ">
                     <div className="row mb-3">
-                        <FormField title="Desplazamientos" name="transportation" role="Kms" vari={toPayTransp} data-testid="transp-input"
-                            value={transportation}
-                            onChange={e => setTransportation(e.target.value)} />
-                        <button type="button" data-testid="transp-reset" className="btn btn-info  btn-sm " onClick={useCallback(() => setTransportation(initialValue), [initialValue])}>Reset</button>    
+                        <div className="input-group  mb-2">
+                            <label className="col-sm-2">Tipo de gasto:</label>
+                            <select className="custom-select col-sm-3" name="kind" onChange={handleChange}>
+                                <option selected>Selecciona una opción</option>
+                                <option value="TRANSPORTATION" >Desplazamientos</option>
+                                <option value="MEAL">Comida</option>
+                                <option value="PARKING">Parking</option>
+                            </select>
+                            <label className="col-sm-2  ml-4">Cantidad:</label>
+                            <input 
+                                data-testid="input"
+                                type="text"
+                                className={`form-control col-sm-2 ${isNaN(values.quantity) ? 'text-danger is-invalid': ''}`} 
+                                name="quantity" 
+                                value={values.quantity} 
+                                onChange={handleChange}/>
+                            <span className="col-sm-2 ml-3 text-right" data-testid="partial-result">A pagar: {payment} $</span>
+                        </div>
+                                                  
                     </div>
-                    
-                    <div className="row mb-3">
-                        <FormField title="Nº de comidas" name="meal" role="Uds." vari={toPayMeal} data-testid="meal-inputl"
-                            value={meal}
-                            onChange={e => setMeal(Math.trunc(e.target.value))} />
-                        <button type="button" data-testid="meal-reset" className=" btn btn-info  btn-sm " onClick={useCallback(() => setMeal(initialValue), [initialValue])}>Reset</button>
-                    </div>
-                    <div className="row mb-3">
-                        <FormField title="Parking" name="parking" role="$" vari={toPayParking} data-testid="parking-input"
-                            value={parking}
-                            onChange={e => setParking(e.target.value)} />
-                        <button type="button" data-testid="parking-reset" className="btn btn-info  btn-sm" onClick={useCallback(() => setParking(initialValue), [initialValue])}>Reset</button>
-                    </div>
-                    <div className="text-right font-weight-bold"  data-testid="total-result">Total a pagar: {toPayTotal} $</div>
+                    {isNaN(values.quantity) && (
+                                <p data-testid="message" className="text-danger">Por favor, introduzca un número válido</p>
+                            )} 
+                    {/* <button type="button" data-testid="transp-reset" className="btn btn-info  btn-sm " onClick={useCallback(() => setValues(initialValues), [initialValues])}>Reset</button>     */}
+                 
+                  
 
                 </div>
             </div>
